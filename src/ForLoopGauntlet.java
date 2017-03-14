15f6a30349ef293e116fe6fb27a886f288435757
Love4653 <league@iMac.attlocal.net>
2017-03-13 19:57:24 -0700

    makeup class with dave 3/13/17
    hi

---
+++ src/ForLoopGauntlet.java
@@ -0,0 +1,35 @@
+import javax.swing.JOptionPane;
+
+public class ForLoopGauntlet {
+public static void main(String[] args) {
+	//for(int i = 0; i<=100; i++){
+	
+	//System.out.println(i);
+	//}
+	//for(int i=100;i>0;i--){
+		//System.out.println(i);
+	//}
+	//for(int i = 0; i<=100; i+=2){
+		
+		//System.out.println(i);
+	//}
+	//for(int i = 1; i < 100; i+=2){
+	//	System.out.println(i);
+	//}
+	//for(int i = 0; i<500; i++){
+		//if(i%2==(1)){
+			//System.out.println(i + " odd");
+		//}
+	//	else{
+		///	System.out.println(i + " even");
+	////	}
+	//}
+	//for(int i = 0; i < 777; i+=7){
+		//System.out.println(i);
+	//}
+int age = 2004;	
+for(int i = 0; i<=12; i++){
+		System.out.println("In" + (age+i) + " I was" + i);		
+	}
+}
+}
