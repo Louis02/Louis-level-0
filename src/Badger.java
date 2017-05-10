37fb9cd191323560407896d26a8bfea0644bf26c
Love4653 <league@iMac-2.attlocal.net>
2017-05-10 16:58:11 -0700

    today is the tenth in may
    hi

---
+++ src/Badger.java
@@ -0,0 +1,36 @@
+
+public class Badger {
+public static void main(String[] args) {
+		for(int i = 0; i<3;i++){
+		bad();
+		System.out.println();
+		mush();
+		System.out.println();
+		}
+		bad();
+		System.out.println();
+		snake();
+		
+	
+}
+static void bad (){
+for(int i = 0; i < 10; i++){
+		
+		
+		
+	
+	System.out.print("badger ");
+}}
+static void mush(){
+for(int i = 0; i < 2; i++){
+		
+		
+		
+	
+	System.out.print("mushroom ");
+}}
+static void snake (){
+	System.out.print("Argh! A snake!");
+}
+
+}
---
+++ src/StringMethods1.java
@@ -0,0 +1,32 @@
+
+
+
+public class StringMethods1 {
+	public static void main(String[] args) {
+		// 1. Create a String variable and initialize it to whatever you want
+String ij = "hey buckeroo how are you, cherry cream on top and whipcream too, wear pants with pleats yes I do, a steam engine goes choo choo choo";
+		// 2. Print the 3rd char of your String to the console.
+		//    HINT: .charAt
+	System.out.print(ij.charAt(131));	
+		// 3. Print the length of your String to the console.
+		//    HINT: .length
+		System.out.println();
+	System.out.print(ij.length());
+		// 4. Using a for loop, print one char at time to the console.
+		//    HINT: use .length() to determine how many loops
+for(int i = 0;i<ij.length();i++){
+	System.out.println(ij.charAt(i));
+}
+		
+		// 5. Pick a char inside your String, and use a loop to determine
+		//    what position/index in the String the char is located.
+		//    Print the char's position to the console.
+		//    EXAMPLE: if your string is "abc" and you are searching 
+		//             for char 'b', then print "b is at index 1"
+		
+	}
+
+
+
+
+}
